3015806bd90f8ec8c55b7d5db3cfb708d4eedae1
Francisco Vásquez <francisjvasquez23@gmail.com>
2025-11-14 14:45:26 -0500

    Avance
    Avance

--- src/config/global.js
+++ src/config/global.js
@@ -329,11 +329,6 @@ export default {
           cargo: 'Evaluador instruccional',
           centro: 'Centro de Comercio y Servicios - Regional Tolima',
         },
-        {
-          nombre: 'Ligia del Rosario Arregocés Osorio',
-          cargo: 'Evaluadora instruccional',
-          centro: 'Centro de Comercio y Servicios - Regional Tolima',
-        },
       ],
     },
     {
